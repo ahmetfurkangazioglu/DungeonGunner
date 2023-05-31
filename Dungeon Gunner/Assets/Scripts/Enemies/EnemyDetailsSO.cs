57dd3d4e732c52f315e24dbd725afd9db2072e67
Ahmet Furkan Gazioğlu <ahmetfurkangazioglu@gmail.com>
2023-06-01 02:31:03 +0300

    Enemy Setup

---
+++ Dungeon Gunner/Assets/Scripts/Enemies/EnemyDetailsSO.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EnemyDetails_", menuName = "Scriptable Objects/Enemy/EnemyDetails")]
+public class EnemyDetailsSO : ScriptableObject
+{
+    #region Header BASE ENEMY DETAILS
+    [Space(10)]
+    [Header("BASE ENEMY DETAILS")]
+    #endregion
+
+    #region Tooltip
+    [Tooltip("The name of the enemy")]
+    #endregion
+    public string enemyName;
+
+    #region Tooltip
+    [Tooltip("The prefab for the enemy")]
+    #endregion
+    public GameObject enemyPrefab;
+}
